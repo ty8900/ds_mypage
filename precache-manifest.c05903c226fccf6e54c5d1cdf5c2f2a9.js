f821b2ce1876494bc6e0ead442d26aaf0b547543
ty8900 <ty8900@naver.com>
2020-08-03 22:57:30 +0900

    Updates

---
+++ precache-manifest.c05903c226fccf6e54c5d1cdf5c2f2a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7d67be989968c9caa0d708d85b2875",
+    "url": "/ds_mypage/index.html"
+  },
+  {
+    "revision": "6445af151341502474f2",
+    "url": "/ds_mypage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20e8115f55a65dda6f7b",
+    "url": "/ds_mypage/static/js/2.d228900c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ds_mypage/static/js/2.d228900c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6445af151341502474f2",
+    "url": "/ds_mypage/static/js/main.b058f232.chunk.js"
+  },
+  {
+    "revision": "78c7786a9c5a48aab8fc",
+    "url": "/ds_mypage/static/js/runtime-main.e38edadb.js"
+  }
+]);
